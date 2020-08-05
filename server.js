9bf80939503c8ac559c1adc5c13a4c8c72098a9e
Carolin Wagner <ich.caro@gmail.com>
2020-08-05 16:47:26 +0200

    Install uuid, add error

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
-const http = require("http");
-const db = require("./db.json");
-
+const http = require('http')
+const fs = require('fs')
+const db = require('./db.json')
+const { v4: uuidv4 } = require('uuid')
 const server = http.createServer((req, res) => {
-  console.log(req.method, req.url);
-
-  if (req.url === "/buddys") {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(db.buddyList));
-  } else {
-    res.statusCode = 404;
-    res.end("Hello client");
+  console.log(req.method, req.url)
+  const routes = {
+    '/buddys': () => {
+      res.setHeader('Content-Type', 'application/json')
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000')
+      res.end(JSON.stringify(db.buddyList))
+    },
+    '/energy': () => {
+      if (req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json')
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000')
+        res.end(JSON.stringify(db.energyData))
+      } else if (req.method === 'POST') {
+        let bodyString = ''
+        req.on('data', (chunk) => (bodyString += chunk))
+        req.on('end', () => {
+          const body = JSON.parse(bodyString)
+          const energyEntry = { ...body, id: uuidv4() }
+          db.energyData.push(energyEntry)
+          fs.writeFile(
+            'db.json',
+            JSON.stringify(db, null, 2),
+            'utf8',
+            (error) => {
+              res.statusCode = error ? 500 : 200
+              const response = error ? error : energyEntry
+              res.end(JSON.stringify(response))
+            }
+          )
+        })
+      }
+    },
+    '404': () => {
+      res.statusCode = 404
+      res.end(
+        '<h1 style="color: crimson">404</h1><p>Sorry, this does not exist</p>'
+      )
+    },
   }
-});
-
+  const route = routes[req.url] || routes['404']
+  route()
+})
 server.listen(4000, () => {
-  console.log("server started on port 4000");
-});
+  console.log('server started on port 4000')
+})
